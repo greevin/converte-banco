c4baef97d048cdcf6285da3b97c489b1fee3c915
Cícero Neto <cicero.alex@gmail.com>
2018-06-11 15:48:07 -0300

    Mudança no arquivo para migrar eventos

--- migra-eventos.php
+++ migra-eventos.php
@@ -3,9 +3,9 @@
 // Horário de Início
 $migrate_start_time_event = '
 INSERT INTO wp_db_nied.wp_db_postmeta (post_id, meta_key, meta_value)
-SELECT node.nid, "horario_de_inicio", SUBSTRING(CAST(REPLACE(field_data_field_hora_inicio.field_hora_inicio_value, "h", ":00") AS TIME), 1, 5) as hora_inicio FROM drupal.node
-LEFT JOIN field_data_field_hora_inicio on node.nid = field_data_field_hora_inicio.entity_id
-WHERE type="evento" AND field_data_field_hora_inicio.field_hora_inicio_value IS NOT NULL;
+SELECT drupal.node.nid, "horario_de_inicio", SUBSTRING(CAST(REPLACE(drupal.field_data_field_hora_inicio.field_hora_inicio_value, "h", ":00") AS TIME), 1, 5) as hora_inicio FROM drupal.node
+LEFT JOIN drupal.field_data_field_hora_inicio on drupal.node.nid = drupal.field_data_field_hora_inicio.entity_id
+WHERE type="evento" AND drupal.field_data_field_hora_inicio.field_hora_inicio_value IS NOT NULL;
 ';
 echo '<b>Adiciona as informações do horário de início do evento</b>'. '<br>';
 execute_query($migrate_start_time_event, $conn, $debug);
@@ -13,9 +13,9 @@ execute_query($migrate_start_time_event, $conn, $debug);
 // Horário de Fim
 $migrate_end_time_event = '
 INSERT INTO wp_db_nied.wp_db_postmeta (post_id, meta_key, meta_value)
-SELECT node.nid, "horario_de_fim", SUBSTRING(CAST(REPLACE(field_data_field_hora_fim.field_hora_fim_value, "h", ":00") AS TIME), 1, 5) as hora_fim FROM drupal.node
-LEFT JOIN field_data_field_hora_fim on node.nid = field_data_field_hora_fim.entity_id
-WHERE type="evento" AND field_data_field_hora_fim.field_hora_fim_value IS NOT NULL;
+SELECT drupal.node.nid, "horario_de_fim", SUBSTRING(CAST(REPLACE(drupal.field_data_field_hora_fim.field_hora_fim_value, "h", ":00") AS TIME), 1, 5) as hora_fim FROM drupal.node
+LEFT JOIN drupal.field_data_field_hora_fim on drupal.node.nid = drupal.field_data_field_hora_fim.entity_id
+WHERE type="evento" AND drupal.field_data_field_hora_fim.field_hora_fim_value IS NOT NULL;
 ';
 echo '<br>'. '<b>Adiciona as informações do horário de fim do evento</b>'. '<br>';
 execute_query($migrate_end_time_event, $conn, $debug);
@@ -23,19 +23,19 @@ execute_query($migrate_end_time_event, $conn, $debug);
 // Data de Início
 $migrate_start_date_event = '
 INSERT INTO wp_db_nied.wp_db_postmeta (post_id, meta_key, meta_value)
-SELECT node.nid, "data_de_inicio", SUBSTRING(REPLACE(field_data_field_data.field_data_value, "-",""), 1, 8) as data_de_inicio FROM drupal.node
-LEFT JOIN field_data_field_data on node.nid = field_data_field_data.entity_id
-WHERE type="evento" AND field_data_field_data.field_data_value IS NOT NULL;
+SELECT drupal.node.nid, "data_de_inicio", SUBSTRING(REPLACE(drupal.field_data_field_data.field_data_value, "-",""), 1, 8) as data_de_inicio FROM drupal.node
+LEFT JOIN drupal.field_data_field_data on drupal.node.nid = drupal.field_data_field_data.entity_id
+WHERE type="evento" AND drupal.field_data_field_data.field_data_value IS NOT NULL;
 ';
 echo '<br>'. '<b>Adiciona as informações do data de início do evento</b>'. '<br>';
 execute_query($migrate_start_date_event, $conn, $debug);
 
 // Data de Fim
 $migrate_end_date_event = '
-INSERT INTO novonied.wp_db_postmeta (post_id, meta_key, meta_value)
-SELECT node.nid, "data_de_fim", SUBSTRING(REPLACE(field_data_field_data_fim.field_data_fim_value, "-",""), 1, 8) AS data_fim FROM drupal.node
-LEFT JOIN field_data_field_data_fim ON node.nid = field_data_field_data_fim.entity_id
-WHERE type="evento" AND field_data_field_data_fim.field_data_fim_value IS NOT NULL;
+INSERT INTO wp_db_nied.wp_db_postmeta (post_id, meta_key, meta_value)
+SELECT drupal.node.nid, "data_de_fim", SUBSTRING(REPLACE(drupal.field_data_field_data_fim.field_data_fim_value, "-",""), 1, 8) AS data_fim FROM drupal.node
+LEFT JOIN drupal.field_data_field_data_fim ON drupal.node.nid = drupal.field_data_field_data_fim.entity_id
+WHERE type="evento" AND drupal.field_data_field_data_fim.field_data_fim_value IS NOT NULL;
 ';
 echo '<br>'. '<b>Adiciona as informações do data de fim do evento</b>'. '<br>';
 execute_query($migrate_end_date_event, $conn, $debug);
@@ -43,19 +43,19 @@ execute_query($migrate_end_date_event, $conn, $debug);
 // Mais Informações
 $migrate_more_info_event = '
 INSERT INTO wp_db_nied.wp_db_postmeta (post_id, meta_key, meta_value)
-SELECT node.nid, "mais_informacoes", CONCAT("", field_data_field_info.field_info_url) FROM drupal.node
-LEFT JOIN field_data_field_info on node.nid = field_data_field_info.entity_id
-WHERE type="evento" AND field_data_field_info.field_info_url IS NOT NULL;
+SELECT drupal.node.nid, "mais_informacoes", CONCAT("", drupal.field_data_field_info.field_info_url) FROM drupal.node
+LEFT JOIN drupal.field_data_field_info on drupal.node.nid = drupal.field_data_field_info.entity_id
+WHERE type="evento" AND drupal.field_data_field_info.field_info_url IS NOT NULL;
 ';
 echo '<br>'. '<b>Adiciona as mais informações sobre o evento</b>'. '<br>';
 execute_query($migrate_more_info_event, $conn, $debug);
 
 // Local
 $migrate_place_event = '
-INSERT INTO wp_db_nied.wp_db_postmeta (post_id, meta_key, meta_value);
-SELECT node.nid, "local", field_data_field_local.field_local_value FROM drupal.node
-LEFT JOIN field_data_field_local on node.nid = field_data_field_local.entity_id
-WHERE type="evento" AND field_data_field_local.field_local_value IS NOT NULL;
+INSERT INTO wp_db_nied.wp_db_postmeta (post_id, meta_key, meta_value)
+SELECT drupal.node.nid, "local", drupal.field_data_field_local.field_local_value FROM drupal.node
+LEFT JOIN drupal.field_data_field_local on drupal.node.nid = drupal.field_data_field_local.entity_id
+WHERE type="evento" AND drupal.field_data_field_local.field_local_value IS NOT NULL;
 
 ';
 echo '<br>'. '<b>Adiciona o local do evento</b>'. '<br>';
--- run.php
+++ run.php
@@ -31,10 +31,10 @@ echo 'Conectado com sucesso'.'<br><br>';
 echo '<br>'.'<b>&gt;&gt;prepara-banco.php</b>'.'<br>';
 require_once "prepara-banco.php";
 
-echo '<br>'. "<b>&gt;&gt;migra-nodes.php</b>" . '<br>';
+echo '<br>'.'<b>&gt;&gt;migra-nodes.php</b>'.'<br>';
 require_once "migra-nodes.php";
 
-echo '<br>'. "<b>&gt;&gt;remove-acentuacao.php</b>" . '<br>';
+echo '<br>'.'<b>&gt;&gt;remove-acentuacao.php</b>'.'<br>';
 require_once "remove-acentuacao.php";
 
 echo '<br>'. "<b>&gt;&gt;migra-arquivos.php</b>" . '<br>';
@@ -43,10 +43,8 @@ require_once "migra-arquivos.php";
 echo '<br>'. "<b>&gt;&gt;migra-equipe.php</b>" . '<br>';
 require_once "migra-equipe.php";
 
-
-//
-//echo '<br>'. "<b>&gt;&gt;migra-eventos.php</b>" . '<br>';
-//require_once "migra-eventos.php";
+echo '<br>'. "<b>&gt;&gt;migra-eventos.php</b>" . '<br>';
+require_once "migra-eventos.php";
 
 
 $conn->close();
