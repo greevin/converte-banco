2adab261e0d7c35a6c1af89d1d1bb0ca7b2ad78d
Cícero Neto <cicero.alex@gmail.com>
2018-06-12 10:43:40 -0300

    Adicionado 'Mais Informações' para um projeto

--- migra-projetos.php
+++ migra-projetos.php
@@ -40,6 +40,17 @@ WHERE type="projeto" and field_email_coord_value IS NOT NULL;
 echo '<br>' . '<b>Adiciona as informações do e-mail do coordenador para um projeto</b>'. '<br>';
 execute_query($migrate_email_coord_info_project, $conn, $debug);
 
+// Mais Informações
+$migrate_more_info_project = '
+INSERT INTO wp_db_nied.wp_db_postmeta (post_id, meta_key, meta_value)
+SELECT drupal.node.nid, "url_projeto", drupal.field_data_field_url_test.field_url_test_url
+FROM drupal.node
+LEFT JOIN drupal.field_data_field_url_test ON node.nid = drupal.field_data_field_url_test.entity_id
+WHERE type="projeto" AND field_url_test_url IS NOT NULL;
+';
+echo '<br>' . '<b>Adiciona as mais informações do projeto</b>'. '<br>';
+execute_query($migrate_more_info_project, $conn, $debug);
+
 // Linha de Pesquisa
 echo '<br>' . '<b>Adiciona a linha de pesquisa para um projeto</b>'. '<br>';
 require_once "carrega-linhas-pesquisa-projetos.php";
