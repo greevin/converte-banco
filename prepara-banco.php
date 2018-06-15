e6aa461d947704f4195eb875b8543d1e9c04a5a3
Cícero Neto <cicero.alex@gmail.com>
2018-06-15 10:01:52 -0300

    Alterado para receber a variável global com o nome da base de dados

--- prepara-banco.php
+++ prepara-banco.php
@@ -3,14 +3,14 @@
 // Limpa as tabelas do Wordpress
 $sql = "
 SELECT * FROM INFORMATION_SCHEMA.TABLES 
-WHERE TABLE_SCHEMA LIKE 'wp_db_nied' 
+WHERE TABLE_SCHEMA LIKE '" . $database . "' 
 AND TABLE_NAME IN ('wp_db_comments', 'wp_db_links', 'wp_db_postmeta', 'wp_db_posts', 'wp_db_term_relationships', 'wp_db_term_taxonomy', 'wp_db_terms');
 ";
 $result = $conn->query($sql);
 $tables = $result->fetch_all(MYSQLI_ASSOC);
 
 foreach($tables as $table) {
-    $sql = 'TRUNCATE TABLE wp_db_nied.'.$table['TABLE_NAME'];
+    $sql = 'TRUNCATE TABLE '. $database . '.' .$table['TABLE_NAME'];
     print_r('Apagando tabela do Wordpress: '.$table['TABLE_NAME'].'<br>');
     $result = $conn->query($sql);
     if ($result == 1) {
@@ -22,46 +22,46 @@ foreach($tables as $table) {
 }
 
 // Cria o ’Sem Categoria’ se ele foi apagado
-$create_no_category = '
-INSERT INTO wp_db_nied.wp_db_terms (name, slug, term_group)
-VALUES ("Sem categoria","sem-categoria", 0);
-';
+$recreate_no_category = '
+INSERT INTO '.$database.".wp_db_terms (name, slug, term_group)
+VALUES ('Sem categoria', 'sem-categoria', 0);
+";
 echo '<br>' . '<b>Recria o "Sem Categoria" se ele foi apagado</b>'. '<br>';
-execute_query($create_no_category, $conn, $debug);
+execute_query($recreate_no_category, $conn, $debug);
 
 // Converte para categorias
 $create_new_categories = '
-REPLACE INTO wp_db_nied.wp_db_terms (name, slug)
+REPLACE INTO '.$database.".wp_db_terms (name, slug)
 SELECT n.name, n.type
 FROM drupal.node_type n;
-';
+";
 echo '<br>' . '<b>Convertendo as categorias</b>'. '<br>';
 execute_query($create_new_categories, $conn, $debug);
 
 // Adiciona os termos na taxonomia
 $add_new_taxonomies = '
-REPLACE INTO wp_db_nied.wp_db_term_taxonomy (term_taxonomy_id, term_id, taxonomy, description, parent)
+REPLACE INTO '.$database.'.wp_db_term_taxonomy (term_taxonomy_id, term_id, taxonomy, description, parent)
 SELECT t.term_id, t.term_id "term_id", "category", "", 0
-FROM wp_db_nied.wp_db_terms t;
+FROM '.$database.'.wp_db_terms t;
 ';
 echo '<br>' . '<b>Adiciona os termos na taxonomia</b>'. '<br>';
 execute_query($add_new_taxonomies, $conn, $debug);
 
 // Associa o conteúdo com os termos
 $associate_term_relationships = '
-INSERT INTO wp_db_nied.wp_db_term_relationships (object_id, term_taxonomy_id)
-SELECT node.nid, wp_db_nied.wp_db_terms.term_id 
+INSERT INTO '.$database.'.wp_db_term_relationships (object_id, term_taxonomy_id)
+SELECT node.nid, '.$database.'.wp_db_terms.term_id
 FROM drupal.node
-INNER JOIN wp_db_nied.wp_db_terms ON wp_db_nied.wp_db_terms.slug = node.type;
+INNER JOIN '.$database.'.wp_db_terms ON '.$database.'.wp_db_terms.slug = node.type;
 ';
 echo '<br>' . '<b>Associa o conteúdo com os termos</b>'. '<br>';
 execute_query($associate_term_relationships, $conn, $debug);
 
 // Atualiza o contador das tags
 $update_tags_counter = '
-UPDATE wp_db_nied.wp_db_term_taxonomy tt
+UPDATE '.$database.'.wp_db_term_taxonomy tt
 SET count = (SELECT COUNT(tr.object_id)
-FROM wp_db_nied.wp_db_term_relationships tr
+FROM '.$database.'.wp_db_term_relationships tr
 WHERE tr.term_taxonomy_id = tt.term_taxonomy_id);
 ';
 echo '<br>' . '<b>Atualiza o contador das tags</b>'. '<br>';
